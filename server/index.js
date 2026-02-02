ddc4c2057f170cd135ffd84e8ab9c8ec5ef81ecf
Shanto93 <shantoislam7363@gmail.com>
2026-02-02 20:33:05 +0600

    Written server actions for different socket events

--- server/index.js
+++ server/index.js
@@ -17,9 +17,22 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log("A user connected: ", socket.id);
+
   socket.on("join_room", (room) => {
     socket.join(room);
     console.log(`${socket.id} joined on room ${room}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("typing", ({ username, room }) => {
+    socket.to(room).emit("message_typing", { username });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
   });
 });
 
