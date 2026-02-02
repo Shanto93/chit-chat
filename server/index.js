dcd6667f9a684bdcbf434ddb7b92c8cd265beae3
Shanto93 <shantoislam7363@gmail.com>
2026-02-02 14:22:29 +0600

    Added connected and joined room status

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const http = require('http');
-const {} = require('socket.io')
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
@@ -11,6 +11,18 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected: ", socket.id);
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`${socket.id} joined on room ${room}`);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
